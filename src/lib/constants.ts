06d70b34127dcf177861a84f9850bbc3e1ce3100
rala-rationallife <s.tsuduki26@gmail.com>
2024-03-29 05:54:48 +0900

    First commit

---
+++ src/lib/constants.ts
@@ -0,0 +1,9 @@
+export const siteMeta = {
+  siteTitle: "RalaCode",
+  siteDesc: "プログラミングの試行錯誤をアウトプットします",
+  siteUrl: import.meta.env.PUBLIC_SITE_URL,
+  siteType: "website",
+  siteLocale: "ja_JP",
+  siteIcon: "/favicon.png",
+  siteImg: "/ogp.png",
+};
