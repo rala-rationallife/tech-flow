7df485b65075a47ba96887da39c9f98f459a597a
rala-rationallife <s.tsuduki26@gmail.com>
2024-04-14 14:02:45 +0900

    Add picture component

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,32 @@
 import { z, defineCollection } from "astro:content";
 import { toZonedTime } from "date-fns-tz";
 
+// const ZodImageMetadata = z.object({
+//   src: z.string(),
+//   width: z.number(),
+//   height: z.number(),
+//   format: z.enum(["jpeg", "jpg", "png", "tiff", "webp", "gif", "svg", "avif"]),
+//   orientation: z.number().optional(),
+// });
+
 const blogCollection = defineCollection({
-  schema: z.object({
-    isDraft: z.boolean().optional(),
-    title: z.string(),
-    pubDate: z
-      .string()
-      .transform((str: string) => toZonedTime(str, "Asia/Tokyo")),
-    modDate: z
-      .string()
-      .transform((str: string) => toZonedTime(str, "Asia/Tokyo"))
-      .optional(),
-    image: z.string(),
-    category: z.array(z.string()),
-    description: z.string(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      isDraft: z.boolean().optional(),
+      title: z.string(),
+      pubDate: z
+        .string()
+        .transform((str: string) => toZonedTime(str, "Asia/Tokyo")),
+      modDate: z
+        .string()
+        .transform((str: string) => toZonedTime(str, "Asia/Tokyo"))
+        .optional(),
+      image: image().refine((img) => img.width >= 1080, {
+        message: "Cover image must be at least 1080 pixels wide!",
+      }),
+      category: z.array(z.string()),
+      description: z.string(),
+    }),
 });
 
 export const collections = {
