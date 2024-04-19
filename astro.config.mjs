418ca2006a08026a39249f9aef49665e74f2346a
rala-rationallife <s.tsuduki26@gmail.com>
2024-04-19 13:47:23 +0900

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],
+  site: "http://localhost:4321",
+  integrations: [react(), mdx(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: "dracula",
