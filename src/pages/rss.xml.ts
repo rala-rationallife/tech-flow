a37b9ec2eb73315cbf253cb01abd1cb2dacfdf90
rala-rationallife <s.tsuduki26@gmail.com>
2024-04-19 13:57:20 +0900

    Add rss

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,18 @@
+import rss, { type RSSOptions } from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { siteMeta } from "@lib/constants";
+
+const { siteTitle, siteDesc } = siteMeta;
+
+export async function get(context: RSSOptions) {
+  const blog = await getCollection("blog");
+  return rss({
+    title: siteTitle,
+    description: siteDesc,
+    site: context.site,
+    items: blog.map((post) => ({
+      ...post.data,
+      link: `/blog/${post.slug}/`,
+    })),
+  });
+}
