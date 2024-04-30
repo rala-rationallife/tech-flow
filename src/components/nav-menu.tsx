57140f77fac73af5ad165a7bf33eee9d57a83679
rala-rationallife <s.tsuduki26@gmail.com>
2024-04-30 12:36:38 +0900

    Add nav menu

---
+++ src/components/nav-menu.tsx
@@ -0,0 +1,97 @@
+import * as React from "react";
+import styles from "@styles/nav-menu.module.css";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Global, css } from "@emotion/react";
+
+export function NavMenu() {
+  const [navIsOpen, setNavIsOpen] = React.useState(false);
+
+  const toggleNav = () => {
+    setNavIsOpen((prev) => !prev);
+  };
+
+  return (
+    <>
+      {navIsOpen && (
+        <Global
+          styles={css`
+            @media (width <= 767px) {
+              body {
+                overflow: hidden;
+              }
+            }
+          `}
+        />
+      )}
+
+      <button className={styles.btn} onClick={toggleNav}>
+        {navIsOpen ? (
+          <>
+            <FontAwesomeIcon icon={faXmark} />
+            <p>とじる</p>
+          </>
+        ) : (
+          <>
+            <FontAwesomeIcon icon={faBars} />
+            <p>メニュー</p>
+          </>
+        )}
+      </button>
+
+      <ul className={navIsOpen ? styles.list_open : styles.list}>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/blog/">Blog</a>
+        </li>
+        <li>
+          <a href="/blog/">Blog</a>
+        </li>
+        <li>
+          <a href="/blog/">Blog</a>
+        </li>
+        <li>
+          <a href="/blog/">Blog</a>
+        </li>
+        <li>
+          <a href="/blog/">Blog</a>
+        </li>
+        <li>
+          <a href="/blog/">Blog</a>
+        </li>
+        <li>
+          <a href="/blog/">Blog</a>
+        </li>
+        <li>
+          <a href="/blog/">Blog</a>
+        </li>
+        <li>
+          <a href="/blog/">Blog</a>
+        </li>
+        <li>
+          <a href="/blog/">Blog</a>
+        </li>
+        <li>
+          <a href="/blog/">Blog</a>
+        </li>
+        <li>
+          <a href="/blog/">Blog</a>
+        </li>
+        <li>
+          <a href="/blog/">Blog</a>
+        </li>
+        <li>
+          <a href="/blog/">Blog</a>
+        </li>
+        <li>
+          <a href="/blog/">Blog</a>
+        </li>
+        <li>
+          <a href="/blog/">Blog</a>
+        </li>
+      </ul>
+    </>
+  );
+}
